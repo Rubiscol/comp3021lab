e624d3fd1a61c3a35bd4ae2b898d1f6fd23ae29d
GU YI <GU YI@10.89.213.139>
2022-02-16 19:29:49 +0800

    second commit

--- 3021/src/comp3021/HelloWorld.java
+++ 3021/src/comp3021/HelloWorld.java
@@ -3,7 +3,7 @@ package comp3021;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("NEW Hello World");
 		// TODO Auto-generated method stub
 
 	}
