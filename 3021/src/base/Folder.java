e12a2a8c749268d9ce5d1f65e62c3e2d83c937c3
GU YI <GU YI@10.89.213.139>
2022-03-10 19:57:42 +0800

    Second

--- 3021/src/base/Folder.java
+++ 3021/src/base/Folder.java
@@ -4,43 +4,48 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.List;
-public class Folder implements Comparable<Folder>{
+
+public class Folder implements Comparable<Folder> {
 	private ArrayList<Note> notes;
 	private String name;
-	
+
 	public Folder(String name) {
-		this.name=new String(name);
-		notes=new ArrayList<Note>();
+		this.name = new String(name);
+		notes = new ArrayList<Note>();
 	}
+
 	public void addNote(Note note) {
 		notes.add(note);
 	}
-	public String getName() {	
+
+	public String getName() {
 		return name;
 	}
-	public ArrayList<Note> getNotes(){	
+
+	public ArrayList<Note> getNotes() {
 		return notes;
 	}
-	
+
 	@Override
 	public String toString() {
-		int nText=0;
-		int nImage=0;
-		for (Note note: notes) {
-			if(note instanceof TextNote) {
+		int nText = 0;
+		int nImage = 0;
+		for (Note note : notes) {
+			if (note instanceof TextNote) {
 				nText++;
-			}
-			else {
+			} else {
 				nImage++;
 			}
 		}
 
-		return name+":"+nText+":"+nImage;
+		return name + ":" + nText + ":" + nImage;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(name);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -52,44 +57,196 @@ public class Folder implements Comparable<Folder>{
 		Folder other = (Folder) obj;
 		return Objects.equals(name, other.name);
 	}
-	
+
 	public int compareTo(Folder o) {
 		return name.compareTo(o.name);
 	}
-	
+
 	public void sortNotes() {
 		Collections.sort(notes);
 	}
-	public List<Note> searchNotes (String keywords){
-	
-		ArrayList<Note> note = new ArrayList<Note>(); 
+
+	public List<Note> searchNotes(String keywords) {
+		ArrayList<Note> note = new ArrayList<Note>();
 		keywords = keywords.toLowerCase();
-		keywords=keywords.replace(" or "," ");
-		String[] wordlists=keywords.split(" ");
-		String first=wordlists[0];
-		String second=wordlists[1];
-		String third=wordlists[2];	
-		String fourth=wordlists[3];
 		for (Note n : notes) {
-			String name=n.getTitle().toLowerCase();
-			if(n instanceof ImageNote) {
-				if (((name.contains(first))||(name.contains(second)))&&((name.contains(third))||(name.contains(fourth)))) {
-					note.add(n);
+			String keyward = new String(keywords);
+			String[] wordlists = keyward.split(" ");
+			boolean findOr = true;
+			// First we check all the or condition. If one of the or condition fail in n,
+			// then we continue to check next n
+			// If all the or conditions pass, all the or and its related elements will be transformed into blank space
+			for (int i = 0; i < wordlists.length; i++) {
+
+				if (wordlists[i].equals("or")) {
+					findOr = false;
+					String name = n.getTitle().toLowerCase();
+					if (n instanceof ImageNote) {
+						if (((name.contains(wordlists[i - 1])) || (name.contains(wordlists[i + 1])))) {
+							wordlists[i - 1] = " ";
+							wordlists[i] = " ";
+							wordlists[i + 1] = " ";
+							findOr = true;
+						}
+					}
+					if (n instanceof TextNote) {
+						String content = n.getContent().toLowerCase();
+						if ((((name.contains(wordlists[i - 1])) || (name.contains(wordlists[i + 1]))))
+								|| (((content.contains(wordlists[i - 1])) || (content.contains(wordlists[i + 1])))))
+
+						{
+							wordlists[i - 1] = " ";
+							wordlists[i] = " ";
+							wordlists[i + 1] = " ";
+							findOr = true;
+						}
+
+					}
+
+					if (findOr == false) {
+						break;
+					}
 				}
 			}
-			else {
-				String content=n.getContent().toLowerCase();
-				if (
-				(((name.contains(first))||(name.contains(second)))&&((name.contains(third))||(name.contains(fourth))))||
-				(((content.contains(first))||(content.contains(second)))&&((content.contains(third))||(content.contains(fourth))))
-				)
-				{
-					note.add(n);
-				}
-				
+			if (findOr == false) {
+				continue;
 			}
-		
+			// Check for the non-blank space element
+			boolean pass = true;
+			for (int x = 0; x < wordlists.length; x++) {
+
+				boolean notcontain1 = true;
+				boolean notcontain2 = true;
+				if (!wordlists[x].equals(" ")) {
+
+					String name = n.getTitle().toLowerCase();
+					if (n instanceof ImageNote) {
+						if (name.contains(wordlists[x])) {
+							notcontain1 = false;
+						}
+					}
+					if (n instanceof TextNote) {
+						String content = n.getContent().toLowerCase();
+						if (name.contains(wordlists[x]) || content.contains(wordlists[x])) {
+							notcontain2 = false;
+						}
+
+					}
+					if (notcontain1 && notcontain2) {
+						pass = false;
+						break;
+					}
+
+				}
+
+			}
+			if (pass == false) {
+				continue;
+			}
+
+			note.add(n);
+
 		}
 		return note;
+
+//		int i = 0;
+//		for (i = 0; i < wordlists.length - 1; i++) {
+//
+//			if (wordlists[i].equals("or")) {
+//				boolean findOr = false;
+//				for (Note n : notes) {
+//					String name = n.getTitle().toLowerCase();
+//					if (n instanceof ImageNote) {
+//						if (((name.contains(wordlists[i - 1])) || (name.contains(wordlists[i + 1])))) {
+//							wordlists[i-1]=" ";
+//							wordlists[i]=" ";
+//							wordlists[i+1]=" ";
+//							findOr = true;
+//							
+//						}
+//					}
+//					if (n instanceof TextNote) {
+//						String content = n.getContent().toLowerCase();
+//						if ((((name.contains(wordlists[i - 1])) || (name.contains(wordlists[i + 1]))))
+//								|| (((content.contains(wordlists[i - 1])) || (content.contains(wordlists[i + 1])))))
+//
+//						{
+//							wordlists[i-1]=" ";
+//							wordlists[i]=" ";
+//							wordlists[i+1]=" ";
+//							findOr = true;
+//						
+//						}
+//
+//					}
+//				}
+//				if (findOr==false) {
+//					return null;
+//				}
+//			}
+//		}
+//		for (i = 0; i < wordlists.length - 1; i++) {
+//			if (!wordlists[i].equals(" ")) {
+//				for (Note n : notes) {
+//					String name = n.getTitle().toLowerCase();
+//					if (n instanceof ImageNote) {
+//						if (name.contains(wordlists[i])) {
+//							findOr = true;
+//							
+//						}
+//					}
+//					if (n instanceof TextNote) {
+//						String content = n.getContent().toLowerCase();
+//						if ((((name.contains(wordlists[i - 1])) || (name.contains(wordlists[i + 1]))))
+//								|| (((content.contains(wordlists[i - 1])) || (content.contains(wordlists[i + 1])))))
+//
+//						{
+//							wordlists[i-1]=" ";
+//							wordlists[i]=" ";
+//							wordlists[i+1]=" ";
+//							findOr = true;
+//						
+//						}
+//
+//					}
+//				}
+//				
+//				
+//				
+//				
+//			}
+//			
+//		}
+
+//		ArrayList<Note> note = new ArrayList<Note>(); 
+//		keywords = keywords.toLowerCase();
+//		keywords=keywords.replace(" or "," ");
+//		String[] wordlists=keywords.split(" ");
+//		String first=wordlists[0];
+//		String second=wordlists[1];
+//		String third=wordlists[2];	
+//		String fourth=wordlists[3];
+//		for (Note n : notes) {
+//			String name=n.getTitle().toLowerCase();
+//			if(n instanceof ImageNote) {
+//				if (((name.contains(first))||(name.contains(second)))&&((name.contains(third))||(name.contains(fourth)))) {
+//					note.add(n);
+//				}
+//			}
+//			else {
+//				String content=n.getContent().toLowerCase();
+//				if (
+//				(((name.contains(first))||(name.contains(second)))&&((name.contains(third))||(name.contains(fourth))))||
+//				(((content.contains(first))||(content.contains(second)))&&((content.contains(third))||(content.contains(fourth))))
+//				)
+//				{
+//					note.add(n);
+//				}
+//				
+//			}
+//		
+//		}
+//		return note;
+
 	}
 }
--- 3021/src/base/NoteBook.java
+++ 3021/src/base/NoteBook.java
@@ -1,83 +1,82 @@
 package base;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 //import com.sun.tools.javac.util.List;
 
-
 public class NoteBook {
-	
+
 	private ArrayList<Folder> folders;
-	
+
 	public NoteBook() {
 		folders = new ArrayList<Folder>();
 	}
-	
-	public boolean createTextNote(String folderName,String title) {
-		TextNote note=new TextNote(title);
-		return insertNote(folderName,note);
-	
+
+	public boolean createTextNote(String folderName, String title) {
+		TextNote note = new TextNote(title);
+		return insertNote(folderName, note);
+
 	}
-	public boolean createImageNote(String folderName,String title) {
-		ImageNote note=new ImageNote(title);
-		return insertNote(folderName,note);
-	
+
+	public boolean createImageNote(String folderName, String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName, note);
+
 	}
-	public ArrayList<Folder>getFolders(){
+
+	public ArrayList<Folder> getFolders() {
 		return folders;
 	}
-	
+
 	public void sortFolders() {
-		for (Folder f:folders) {
+		for (Folder f : folders) {
 			f.sortNotes();
-			}
+		}
 		Collections.sort(folders);
-		}
-	
-	public boolean insertNote(String foldername,Note note) {
-		
-		Folder folder=null;
-		for (Folder f:folders) {
+	}
+
+	public boolean insertNote(String foldername, Note note) {
+
+		Folder folder = null;
+		for (Folder f : folders) {
 			if (f.getName().equals(foldername)) {
-				folder=f;
+				folder = f;
 			}
 		}
-		if (folder==null) {
-			folder= new Folder(foldername);
+		if (folder == null) {
+			folder = new Folder(foldername);
 			folders.add(folder);
 		}
-		
+
 		for (Note n : folder.getNotes()) {
 			if (n.equals(note)) {
-			System.out.println("Creating note "+note.getTitle()+" under folder "+foldername+" failed");
-			return false;
+				System.out.println("Creating note " + note.getTitle() + " under folder " + foldername + " failed");
+				return false;
 			}
-			}
+		}
 		folder.addNote(note);
 		return true;
-			
-		}
-	
 
-	
-	public List<Note> searchNotes(String keywords){
-		ArrayList<Note> note = new ArrayList<Note>(); 
-		for (Folder f:folders) {
-			if (!(f.searchNotes(keywords).isEmpty())) {
-				note.addAll(f.searchNotes(keywords));
+	}
+
+	public List<Note> searchNotes(String keywords) {
+		ArrayList<Note> note = new ArrayList<Note>();
+		for (Folder f : folders) {
+			if (f.searchNotes(keywords) != null) {
+				if (!(f.searchNotes(keywords).isEmpty())) {
+					note.addAll(f.searchNotes(keywords));
+				}
 			}
 		}
 		return note;
-		
-	}
-	
-	public boolean createTextNote(String folderName,String title,String content) {
-		TextNote note=new TextNote(title,content);
-		return insertNote(folderName,note);
-		
-	}
 
 	}
-	
 
+	public boolean createTextNote(String folderName, String title, String content) {
+		TextNote note = new TextNote(title, content);
+		return insertNote(folderName, note);
 
+	}
+
+}
